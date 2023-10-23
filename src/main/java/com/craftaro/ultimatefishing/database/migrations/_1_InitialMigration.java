153c8b060a95a061162e70d03b41de1107a883be
Christian Koop <contact@sprax2013.de>
2023-10-23 17:02:18 +0200

    Make database migrations compatible with latest CraftaroCore snapshot

--- src/main/java/com/craftaro/ultimatefishing/database/migrations/_1_InitialMigration.java
+++ src/main/java/com/craftaro/ultimatefishing/database/migrations/_1_InitialMigration.java
@@ -1,22 +1,20 @@
 package com.craftaro.ultimatefishing.database.migrations;
 
 import com.craftaro.core.database.DataMigration;
-import com.craftaro.core.database.DatabaseConnector;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class _1_InitialMigration extends DataMigration {
-
     public _1_InitialMigration() {
         super(1);
     }
 
     @Override
-    public void migrate(DatabaseConnector databaseConnector, String tablePrefix) throws SQLException {
+    public void migrate(Connection connection, String tablePrefix) throws SQLException {
         // Create caught table
-        try (Statement statement = databaseConnector.getConnection().createStatement()) {
+        try (Statement statement = connection.createStatement()) {
             statement.execute("CREATE TABLE " + tablePrefix + "caught (" +
                     "uuid VARCHAR(36) NOT NULL, " +
                     "rarity TEXT NOT NULL," +
@@ -25,5 +23,4 @@ public class _1_InitialMigration extends DataMigration {
 
         }
     }
-
 }
